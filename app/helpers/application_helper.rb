27f7270b22c068ac522e65b2400bad5a3749a2c2
pigate <mimchiang@gmail.com>
2014-11-25 20:02:47 -0800

    Recipe's ingreds and steps now show on show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,3 @@
 module ApplicationHelper
+  include RecipesHelper
 end
--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,10 @@
+require 'json'
 module RecipesHelper
+  def destringify(string_json)
+    if (string_json)
+      JSON.parse(string_json)
+    else
+      []
+    end
+  end
 end
