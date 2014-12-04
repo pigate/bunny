32f05c2fc070bab2eff86b80ab6c426c0f5c3a94
pigate <mimchiang@gmail.com>
2014-12-03 17:01:53 -0800

    Bug: missing : bucket. Revisiting app.rb. Moved bucket out of credentials

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,10 @@ module Bunny
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_protocol => 'http',
+    :bucket => ENV['AWS_BUCKET'],
     :s3_credentials => {
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :bucket => ENV['AWS_BUCKET']
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
     :url => ':s3_domain_url',
     :path => '/:class/:attachment/:id_partition/:style/:filename'
