aa38fb26848e87b2ff44909e87539f4d7227b2fd
pigate <mimchiang@gmail.com>
2014-12-07 14:47:05 -0800

    Changed json format double quote to U+022

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,7 +1,8 @@
 require 'json'
 module RecipesHelper
   def destringify(string_json)
-    if (string_json && /[A-Za-z]/.match(string_json))
+    if (string_json != '' && string_json != nil)
+#    if (string_json && /[A-Za-z]/.match(string_json))
       JSON.parse(string_json)
     else
       []
