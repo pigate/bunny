029b244969e674ae6b88ee16e9a7388e56e07932
pigate <mimchiang@gmail.com>
2014-12-10 23:10:58 -0800

    Fleshed out posts a litle more

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,17 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-
+  before_action :signed_in, only: [:new, :create, :edit, :update, :destroy]
+  before_action :can_edit, only: [:edit, :update, :destroy]
   respond_to :html
 
   def index
     @posts = Post.all
-    respond_with(@posts)
+    if (params[:search].present?)
+      @posts = Post.search(params[:search]).records.to_a
+    else
+      @posts = Post.all
+      respond_with(@posts)
+    end
   end
 
   def show
@@ -22,7 +28,10 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.save
+    @post.author_id = current_member.id
+    if @post.save
+      @post.convo = Convo.new(:conversable_id => @post.id, :conversable_type => "Post", :owner_id => current_member.id)
+    end
     respond_with(@post)
   end
 
@@ -37,11 +46,22 @@ class PostsController < ApplicationController
   end
 
   private
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    def post_params
-      params.require(:post).permit(:author_id, :feed_id, :post_text, :s_tags)
+  def post_params
+    params.require(:post).permit(:author_id, :post_text, :s_tags, :photo)
+  end
+  def signed_in
+    if !member_signed_in?
+      redirect_to members_login_path
     end
+  end
+  def can_edit
+    if !(current_member.id == @post.author_id || current_member.admin == true)
+      redirect_to not_found_path
+    end
+  end
+
 end
